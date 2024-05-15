c0540d23b7652e7b0fbf1d31ca506b1e1d739fa2
Abubakar Rasheed <131538521+abubakarrasheed@users.noreply.github.com>
2024-05-15 23:40:15 +0500

    Update index.js

--- index.js
+++ index.js
@@ -28,5 +28,52 @@ while(i<=5){
     i++;
 }
 
-__________________________//DO WHILE LOOP
+__________________________//DO WHILE LOOP  (aik bar chlega, agr condition glt v ho)
 
+let j=20;
+do{
+    console.log("Abu Bakar");
+    i++;
+} while(j<=5);
+
+
+____________________________//FOR- OF - LOOP 
+
+let str="AbuBakar Rasheed"
+let size = 0; // for size check
+for(let value of str){
+    console.log("Value is =" + value);
+    size++;
+}
+console.log("Size is =" + size);
+
+____________________________//FOR- IN - LOOP  (used in objects & arrays)
+
+let student={
+    name: "AbuBakar",
+    age:23,
+    cgpa:3.5,
+    isPss:true
+};
+for(let key in student){
+    console.log("Key=" , key, "Value=" ,student [key]);
+}
+
+___________________________//Q=1, Print All even number from 0 to 100
+
+let num;
+for(let num=0 ;num<=100 ; num++){
+    if(num%2==0){
+        console.log(num);
+    }
+}
+
+___________________________//Q=2 GUESS NUMBER GAME
+
+let numb=25;
+let userNum=("Enter a number");
+
+while(userNum !== numb ){
+    userNum=prompt("Sorry , Enter again write number");
+}
+console.log("Congratulations, You enter a right number");
