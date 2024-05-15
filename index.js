e2a28c1181456396b56719db0e66a27d3c416822
Abubakar Rasheed <131538521+abubakarrasheed@users.noreply.github.com>
2024-05-15 09:25:05 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+__________________________//FOR LOOP
+
+__//Print 1 to 5
+for(let i=1; i<=5 ; i++){
+    console.log("Abu bakar rashhed");
+}
+
+__//Calculate sum of 1 to 5
+
+let sum=0;
+for(i=1 ; i<=5; i++){
+    sum=sum+i;
+}
+
+console.log("The sum is=" +sum);
+
+__//Infinite Loop
+
+for(let i=1; i>=0; i++ ){
+    console.log("Abu Bakar Rasheed");
+}
+
+__________________________//WHILE LOOP
+
+let i=1;
+while(i<=5){
+    console.log(i);
+    i++;
+}
+
+__________________________//DO WHILE LOOP
+
